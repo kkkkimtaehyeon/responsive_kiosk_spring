6e047174a87ec6b0a02e16e2dd6ce0a4c4af35f0
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2024-04-08 15:21:03 +0900

    initial commit

---
+++ src/main/java/com/example/responsive_kiosk/ResponsiveKioskApplication.java
@@ -0,0 +1,13 @@
+package com.example.responsive_kiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponsiveKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponsiveKioskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/responsive_kiosk/ResponsiveKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.responsive_kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponsiveKioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
