d2c090503e84bcaee6ab2b3c32ccec7f818ad50d
kkkkimtaehyeon <kth1927@naver.com>
2024-04-23 22:36:32 +0900

    생성시간 엔티티 생성

---
+++ src/main/java/com/example/responsive_kiosk/common/entity/BaseTimeEntity.java
@@ -0,0 +1,18 @@
+package com.example.responsive_kiosk.common.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createTime;
+}
