1d375a282cf73b54c3cf77cb26d779e5054de027
kkkkimtaehyeon <kth1927@naver.com>
2024-04-30 01:56:27 +0900

    CORS 설정

---
+++ src/main/java/com/example/responsive_kiosk/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.responsive_kiosk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000"); // 리액트 애플리케이션의 출처
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
